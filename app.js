317f5fb90601f6dd5a50ffb455f38096f3f02220
Vasu Sachdeva <vasusachdeva77@gmail.com>
2023-12-25 17:28:12 +0530

    implemented read & write in database, and corresponding API endpoints too

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 const { initializeApp } = require("firebase/app");;
-const { getDatabase, ref, set} = require("firebase/database");
+const { getDatabase, ref, set, onValue, remove} = require("firebase/database");
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -9,11 +9,11 @@ const tess = require("tesseract.js");
 require('dotenv').config();
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+const bodyParser = require('body-parser');
+const multer = require("multer");
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = initializeApp({
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: "thai-id-ocr.firebaseapp.com",
@@ -25,6 +25,22 @@ const firebaseConfig = initializeApp({
     measurementId: "G-4BEGLQR46B"
 });
 
+async function writeUserData(id, name, lastName, dateOfBirth, dateOfExpiry, dateOfIssue) {
+    try{
+        const db = await getDatabase();
+        await set(ref(db, 'users/' + id), {
+        firstName: name,
+        lastName: lastName,
+        dateOfBirth: dateOfBirth,
+        dateOfExpiry: dateOfExpiry,
+        dateOfIssue: dateOfIssue
+        });
+        console.log("WRITTEN TO DATABASE",id);
+    }
+    catch(error){
+        console.log("ERROR WHILE WRITING:", error);
+    }
+}
 
 app.get("/", function (req, res) {
     res.render("index", {
@@ -36,10 +52,7 @@ app.get("/", function (req, res) {
         dateOfBirth: 'Not found'
     });
 });
-// Multer configuration for handling file uploads
-const multer = require("multer");
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+
 
 // Route to handle the POST request for image upload
 app.post("/upload", upload.single("fileInput"), (req, res) => {
@@ -83,8 +96,8 @@ app.post("/upload", upload.single("fileInput"), (req, res) => {
             const dateOfBirth = extractInfo('Date of Birth');
 
             // Log the extracted information
+            writeUserData(identificationNumber, name, lastName, dateOfBirth, dateOfExpiry, dateOfIssue);
             console.log(ocrText);
-
             // Send the extracted information and OCR text as part of the response
             return res.render("index", {
                 name,
@@ -102,10 +115,40 @@ app.post("/upload", upload.single("fileInput"), (req, res) => {
         });
 });
 
-
-
-// Initialize Firebase
-app.listen(4000, function () {
-    console.log(`Running on port 4000`);
+app.get("/get-all-users", async (req, res) => {
+    try {
+        const dbRef = ref(getDatabase(), 'users/');
+        onValue(dbRef, async (snapshot) => {
+            const data = await snapshot.val();
+            console.log("data of all users:", data);
+            const records = data || {}; // Use an empty object if data is null
+            console.log("ACTION SUCCESSFUL");
+            res.render("index2", {records: records});
+        });
+    }
+    catch (error) {
+        console.error('ERROR FETCHING USERS:', error);
+        return res.status(500).json({ success: false, message: "Internal server error" });
+    }
 });
-  
+
+app.delete("/delete-record/:id", async(req, res) => {
+    try{
+        console.log("id to be deleted:",req.params.id);
+        const db = getDatabase();
+        const dbRef = ref(db, 'users/'+req.params.id);
+        await remove(dbRef);
+        console.log("DELETE SUCCESSFUL:", req.params.id);
+        res.redirect("/get-all-users");
+    }
+    catch(error){
+        console.log("ERROR WHILE DELETING:", error);
+        res.status(200).json({error: "Error while deleting"});
+    }
+});
+
+
+const port = parseInt(process.env.PORT, 10) || 4000;
+app.listen(port, function () {
+  console.log(`Running on port ${port}`);
+});
